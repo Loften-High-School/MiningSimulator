7c690d53d603198dbf17508e4cd85beba08da7f8
ThyRedRock <kolicannon@gmail.com>
2026-04-03 09:23:53 -0400

    FINISHED
    working menu basic exit and start game functionality

--- Assets/Scripts/StartMenuController.cs
+++ Assets/Scripts/StartMenuController.cs
@@ -6,4 +6,16 @@ using UnityEngine.SceneManagement;
 public class StartMenuController : MonoBehaviour
 {
    
+   public void OnStartClick()
+   {
+        SceneManager.LoadScene("SampleScene");
+   }
+
+   public void OnExitClick()
+   {
+       #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false; 
+        #endif
+            Application.Quit();
+   }
 }
